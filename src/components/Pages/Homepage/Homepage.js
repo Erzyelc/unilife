7e9ca99df778c7a29de4b576a406e411d70e36a2
Ryan Carpenter <ryancarpenter@mac-mini.myfiosgateway.com>
2023-01-10 15:55:47 -0500

    added styling to homepage

--- src/components/Pages/Homepage/Homepage.js
+++ src/components/Pages/Homepage/Homepage.js
@@ -5,6 +5,12 @@ import CityCard from '../../CityCard/CityCard';
 import globe from '../../../assets/globe.png';
 import compare from '../../../assets/compare.png';
 import bills from '../../../assets/bills.png';
+import person from '../../../assets/person.png'
+import heart from '../../../assets/heart.png'
+import handhome from '../../../assets/handhome.png'
+import facebook from '../../../assets/facebook.png'
+import twitter from '../../../assets/twitter.png'
+import instagram from '../../../assets/instagram.png'
 
 function Homepage() {
 
@@ -38,22 +44,65 @@ function Homepage() {
           /*cities.map(item => <p>{item.name}</p>)*/
         }
         </div>
-        <button>See All Cities</button>
+        <div className="cities-btn-container">
+          <button className="cities-btn">See All Cities</button>
+        </div>
         <div class="compare-container">
           <h2>Compare all inclusive student homes.</h2>
-          <div compare-items-container>
-          <div>
+          <div className= "compare-items-container">
+          <div className="compare-item">
             <img src={globe} alt="" className="compare-icon"/>
             <h3>Search</h3>
+            <p>Find your dream home in the perfect area near your university.</p>
           </div>
-          <div>
+          <div className="compare-item">
             <img src={compare} alt="" className="compare-icon"/>
             <h3>Compare</h3>
+            <p>Compare student accomodation to find the right home for you.</p>
           </div>
-          <div>
+          <div className="compare-item">
             <img src={bills} alt="" className="compare-icon"/>
             <h3>Bills Included</h3>
+            <p>Bills are included in all rent prices. No hidden fees.</p>
           </div>
+          </div>
+        </div>
+        <div className="search-container">
+          <div className="selection-container">
+          <div className="selection-item">
+          <img src={heart} alt="#"/>
+          <div className="selection-content">
+            <h3>Best selection</h3>
+            <p>Best selection of student accomodations.<br></br> Never been easier to find a home that's <br></br>right for you.</p>
+          </div>
+          </div>
+          <div className="selection-item">
+            <img src={handhome} alt="#"/>
+            <div className="selection-content">
+              <h3>Your favorite</h3>
+              <p>Shortlist your favourite properties and send <br></br>enquiries in one click.</p>
+            </div>
+          </div>
+            <button>Search & Compare</button>
+          </div>
+          <div className="person">
+            <img src={person} alt="#"/>
+          </div>
+        </div>
+        <div className="contact-container">
+          <div className="connect-container">
+            <h2>Keep in touch</h2>
+            <p>Curious about new offerings? Sign up for our weekly newsletter and stay informed</p>
+            <input 
+              type="text"
+              placeholder="Your email"
+              />
+          </div>
+          <div className="socialize-container">
+            <h2>Let's Socialize</h2>
+            <img src={facebook} alt="#"/>
+            <img src={twitter} alt="#"/>
+            <img src={instagram} alt="#"/>
           </div>
         </div>
     </div>

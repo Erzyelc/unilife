3abf35d6a87ef0852dc9e02be06546f1a61f8820
Ryan Carpenter <ryancarpenter@mac-mini.myfiosgateway.com>
2023-01-10 23:57:45 -0500

    continued styling homepage

--- src/components/Pages/Homepage/Homepage.js
+++ src/components/Pages/Homepage/Homepage.js
@@ -92,17 +92,28 @@ function Homepage() {
         <div className="contact-container">
           <div className="connect-container">
             <h2>Keep in touch</h2>
-            <p>Curious about new offerings? Sign up for our weekly newsletter and stay informed</p>
+            <p>Curious about new offerings? Sign up for our <br></br>weekly newsletter and stay informed</p>
             <input 
+              className="connect-input"
               type="text"
               placeholder="Your email"
               />
           </div>
           <div className="socialize-container">
-            <h2>Let's Socialize</h2>
-            <img src={facebook} alt="#"/>
-            <img src={twitter} alt="#"/>
-            <img src={instagram} alt="#"/>
+          <h2>Let's Socialize</h2>
+            <div className="social-item">
+              <img src={facebook} alt="#"/>
+              <h4><a href="#">Facebook</a></h4>
+            </div>
+            <div className="social-item">
+              <img src={twitter} alt="#"/>
+              <h4><a href="#">Twitter</a></h4>
+            </div>
+            <div className="social-item">
+              <img src={instagram} alt="#"/>
+              <h4><a href="#">Instagram</a></h4>
+            </div>
+            
           </div>
         </div>
     </div>
